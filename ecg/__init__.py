13d3fe0dbc7a994b02b838eeaaa0d970c889bc34
Harrison Smith <hbs@asu.edu>
2018-04-04 10:36:24 -0700

    added imports for scraping

--- ecg/__init__.py
+++ ecg/__init__.py
@@ -2,3 +2,5 @@
 # Use of this source code is governed by a MIT
 # license that can be found in the LICENSE file.
 from .convert_eclists_to_graphs import write_graphs_from_one_genome, write_graphs_from_many_genomes
+from scrape_eukarya_from_jgi import scrape_eukarya_from_jgi
+from scrape_metagenomes_from_jgi import scrape_metagenomes_from_jgi
