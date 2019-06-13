62a93bd1799ecc33611ac3e54b40a026aee6821f
Harrison Smith <hbs@asu.edu>
2019-06-13 12:14:19 +0900

    more outlines for testing

--- test/test_updating.py
+++ test/test_updating.py
@@ -1,8 +1,14 @@
+import unittest
+import os
+import ecg
+
 ## Test kegg.update release change
 ## - if current release ("test") doesn't match retrieved release, update and 
 ##   check to make sure keys in the version.json and master.json match what 
 ##   they should be
 ## - test second update to verify it works the same way
+
+# class TestKeggUpdateReleaseChange(unittest.TestCase):
 
 ## Test kegg.update list change
 ## - if retrieved lists added key compared to the current lists, check to make sure
